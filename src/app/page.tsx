ad35753656eb8b8d802b02d70164da0f16bed380
Ricardo Acosta <ricardo.acosta1809@gmail.com>
2024-08-31 15:45:46 -0600

    improve build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/4079e598-5d88-448e-b332-59aeac91f42f-7wf0cv.jpeg",
+  "https://utfs.io/f/50e4ad39-cf39-414d-a10e-2506266e1d8a-6jqkjc.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
